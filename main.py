5b470322f8e7a76f8fe2fce061c5ca176e2c6656
ZhanMaster314 <thesecondcoming82@gmail.com>
2026-04-18 13:05:16 +0500

    determinant scene

--- main.py
+++ main.py
@@ -26,132 +26,13 @@ class demo(Scene):
         i_hat.save_state()
         j_hat.save_state()
 
-        c = math.cos(math.pi / 4)
-        s = math.sin(math.pi / 4)
-
-        target_matrix = [[2, -1], [1, 2]]
-        forty_five_clockwise_matrix=[[c, s], [-s, c]]
-
-        
-        one_matrix_tex = Matrix([[1, 0], [0, 1]]).add_background_rectangle()
-        one_matrix_tex.to_corner(UR) # UR = Upper Right
-        one_matrix_tex.save_state()
-        
-        target_matrix_tex = Matrix([[2, -1], [1, 2]]).add_background_rectangle()
-        target_matrix_tex.to_corner(UR)
-
-        forty_five_clockwise_matrix_tex=Matrix([
-            [r"\cos(\pi/4)", r"\sin(\pi/4)"],
-            [r"-\sin(\pi/4)", r"\cos(\pi/4)"]
-        ], h_buff=2.5).add_background_rectangle()
-        forty_five_clockwise_matrix_tex.next_to(target_matrix_tex,LEFT)
-
+        #INITIAL STAGE
         self.add(i_hat)
         self.add(j_hat)
-        self.wait(1)
-        self.play(Create(plane), run_time=3)
-        self.wait(0.5)
-        self.play(
-            Write(i_label)
-        )
-        self.play(
-            Write(j_label)
-        )
-        self.wait(0.5)
-        self.play(
-            Write(one_matrix_tex)
-        )
-        self.wait(3)
-
-        #THROW DOWN
-        self.play(
-            ReplacementTransform(one_matrix_tex, target_matrix_tex),
-        )
-        self.play(
-            plane.animate.apply_matrix(target_matrix),
-            i_hat.animate.apply_matrix(target_matrix),
-            j_hat.animate.apply_matrix(target_matrix),            
-            run_time=2
-        )
-        self.wait(2)
-
-        #throw up
-        self.play(
-            Write(forty_five_clockwise_matrix_tex)
-        )
-        self.wait(1)
+        self.add(plane)
+        self.add(i_label)
+        self.add(j_label)
         
-        self.play(
-            plane.animate.apply_matrix(forty_five_clockwise_matrix),
-            i_hat.animate.apply_matrix(forty_five_clockwise_matrix),
-            j_hat.animate.apply_matrix(forty_five_clockwise_matrix),
-            run_time=0.5
-        )
-
-        #Back to normal
-        matrix_group = VGroup(forty_five_clockwise_matrix_tex, target_matrix_tex)
-        brackets = MathTex(r"\left[", r"\right]")
-        brackets.height = matrix_group.height + 0.5 # Auto-size to matrices
-        brackets[0].next_to(matrix_group, LEFT, buff=0.2)
-        brackets[1].next_to(matrix_group, RIGHT, buff=0.2)
-
-        inverse_power = MathTex(r"-1").scale(0.9) # Small and tiny
-        inverse_power.next_to(brackets[1].get_corner(UR), RIGHT, buff=0.05).shift(UP*0.2)
-        
-        full_inv_expression = VGroup(matrix_group, brackets, inverse_power)
-        
-
-        self.play(
-            Write(inverse_power),
-            Write(brackets),
-            full_inv_expression.animate.shift(LEFT * 1.5 + DOWN * 0.5),
-        )
-        self.wait(1)
-        self.play(
-            ReplacementTransform(full_inv_expression, one_matrix_tex),
-            Restore(one_matrix_tex),
-
-            run_time=1,
-            rate_func=smooth  
-        )
-        self.play(
-            Restore(plane),
-            Restore(i_hat),
-            Restore(j_hat),
-            run_time=0.5
-        )
-        self.wait(3)
-
-        #TREADMILL
-        treadmill_matrix=[[100,0],[0,1]]
-        treadmill_matrix_tex = Matrix([[100,0],[0,1]]).add_background_rectangle().to_corner(UR)
-        
-
-        self.play(
-            ReplacementTransform(one_matrix_tex, treadmill_matrix_tex),
-        )
-        self.play(
-            plane.animate.apply_matrix(treadmill_matrix),
-            i_hat.animate.apply_matrix(treadmill_matrix),
-            run_time=10,
-            rate_func=rush_from  
-
-        )
-        self.play(
-            Restore(one_matrix_tex),
-            ReplacementTransform(treadmill_matrix_tex,one_matrix_tex)
-        )
-        self.play(
-            Restore(plane),
-            Restore(i_hat),
-            Restore(j_hat),
-            run_time=0.5
-        )
-        self.wait(3)
-
-        #1 CREATES DOT PRODUCT GUN
-        self.play(Unwrite(one_matrix_tex), run_time=1)
-        #CREATION
         v_coords = np.array([4, 1, 0])
         w_coords = np.array([2, -1, 0])
         
@@ -159,62 +40,58 @@ class demo(Scene):
         vec_w = Vector(w_coords, color=PINK)
 
         v_label = MathTex(r"\vec{v}", color=YELLOW).next_to(vec_v.get_end(), RIGHT)
-        w_label = MathTex(r"\vec{w}", color=PINK).next_to(vec_w.get_end(), RIGHT)
         
         dot_equation = MathTex(
-            r"\begin{bmatrix} 4 \\ 1 \end{bmatrix}", 
-            r"\cdot", 
+            r"\begin{bmatrix} 4 & 1 \end{bmatrix}",
+            
             r"\begin{bmatrix} 2 \\ -1 \end{bmatrix}",
-        ).to_edge(UP, buff=0.5).add_background_rectangle()
+        ).to_edge(UP, buff=0.5)
+        dot_equation[0].set_color(YELLOW) # The [4, 1] part
+        dot_equation[1].set_color(PINK)   # The [2, -1] part
 
-        dot_equation[1].set_color(YELLOW) # The [4, 1] part
-        dot_equation[3].set_color(PINK)   # The [2, -1] part
-
-        self.play(
-            GrowArrow(vec_v),
-            Write(v_label)
-        )
-        self.play(
-            GrowArrow(vec_w),
-            Write(w_label)
-        )
-        self.wait(1)
-        self.play(Write(dot_equation))
-        self.wait(2)
+        self.add(vec_v)
+        self.add(v_label)
+        self.add(dot_equation)
+    
         #PROJECTION 
         unit_v = v_coords / np.linalg.norm(v_coords)
         projection_point = np.dot(w_coords, unit_v) * unit_v
+        w_label = MathTex(r"\vec{w}", color=PINK).next_to(projection_point, DR, buff=0.1)
+        vec_w.put_start_and_end_on(ORIGIN, projection_point),
 
-        proj_line = DashedLine(
-            start=vec_w.get_end(), 
-            end=projection_point, 
-            color=GRAY,
-            stroke_width=2
-        )
-        horizontal_v = MathTex(r"\begin{bmatrix} 4 & 1 \end{bmatrix}").next_to(dot_equation[3], LEFT, buff=0.1)
-        horizontal_v.set_color(YELLOW)
-
-        self.play(Create(proj_line))
-        self.wait(0.5)
-        self.play(
-            FadeOut(dot_equation[2]),
-            ReplacementTransform(dot_equation[1], horizontal_v),
-
-        )
-        self.play(
-            vec_w.animate.put_start_and_end_on(ORIGIN, projection_point),
-            w_label.animate.next_to(projection_point, DR, buff=0.1),
-            run_time=3,
-            rate_func=smooth
-
-        )
-        self.play(
-            FadeOut(proj_line),
-            FadeOut(dot_equation),
-            FadeOut(horizontal_v)
-            )
+        self.add(vec_w)
+        self.add(w_label)
         
-        self.wait(22)
+        self.wait(2)
+
+        #ACTION
+        self.play(
+            FadeOut(vec_v),
+            FadeOut(vec_w),
+            FadeOut(v_label),
+            FadeOut(w_label)
+
+        )
+        cross_product_eq=MathTex(
+            r"\begin{bmatrix} 1 \\ 0 \end{bmatrix}",
+            r"\times" ,
+            r"\begin{bmatrix} 0 \\ 1 \end{bmatrix}",
+            r"= 1"
+        ).to_edge(UP, buff=0.5).add_background_rectangle()
+        self.play(
+            ReplacementTransform(dot_equation, cross_product_eq),
+        )
+        self.wait(2)
+        
+        
+        det_eq = MathTex(r"det \begin{bmatrix} 1 & 0 \\ 0 & 1 \end{bmatrix} =1").to_edge(UP, buff=0.5).add_background_rectangle()
+        self.play(
+            ReplacementTransform(cross_product_eq, det_eq),
+        )
+        self.wait(1)
+        
+
+
 
 
         
