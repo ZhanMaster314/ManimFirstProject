737c4a40bad995f030640c65454e98fca9411be1
ZhanMaster314 <thesecondcoming82@gmail.com>
2026-04-23 16:50:48 +0500

    sce11

--- main.py
+++ main.py
@@ -18,23 +18,7 @@ class demo(Scene):
         radius=1.5
         num_rings = 15
     
-        circle = Circle(radius=radius, 
-                        color=WHITE,  
-                        fill_color=BLUE,
-                        fill_opacity=0.5,         
-                        ).shift(LEFT * 5)
-        self.add(circle)
-
-        right_amount = 1.5
-        up_amount = 2
-        target_position = circle.get_center() + [right_amount, up_amount, 0]
         
-        label = MathTex("A")
-        label.scale(1.5)
-        label.move_to(target_position)
-        self.add(label)
-        
-
         #BARS INTRO
         thickness = radius / num_rings
         bars = VGroup()
@@ -77,27 +61,8 @@ class demo(Scene):
         self.add(x_label)
         self.add(y_label)
 
-        fake_bars_on_axes=VGroup(*[b.copy() for b in bars])
-        fake_bars_on_axes.arrange(RIGHT, buff=0, aligned_edge=DOWN)
-
-        origin_coord = axes.c2p(0, 0)
-        fake_bars_on_axes.next_to(origin_coord, UR, buff=0, aligned_edge=DOWN)
         
-        for i in range(num_rings):
-            bars[i].move_to(fake_bars_on_axes[i].get_center())
-            self.add(bars[i])
-        
-
-        largest_bar = bars[-1]
-        c_label = MathTex(r"2\pi R", color=YELLOW)
-        c_label.next_to(largest_bar, RIGHT, buff=0.2)
-        
-        r_label = MathTex("R", color=YELLOW)
-        r_label.next_to(bars.get_bottom(), DOWN, buff=0.2)
-
-        self.add(c_label)
-        self.add(r_label)
-        
+        origin_coord = axes.c2p(0, 0) 
         
         y_shrinking_group = VGroup(bars, axes.get_y_axis())
         y_scale_factor = 1 / (2 )
@@ -107,60 +72,23 @@ class demo(Scene):
             )
 
         
-        line = axes.plot(
-            lambda x: 2 * np.pi * x, 
-            x_range=[0, 9], 
-            color=YELLOW
-        )
-        
-        #line_label = MathTex(r"y = 2\pi x", color=YELLOW)
-        line_label=MathTex(r"y", r"=", r"2", r"\pi", r"x", color=YELLOW)
-        line_label.shift(LEFT)
-
-        self.add(line)
-        self.add(line_label)
-        
-
-        
-        A_eq_two_tex=MathTex(r"A=\frac{1}{2} R \cdot 2\pi R")
-        A_eq_two_tex.shift(RIGHT*2.5)
-        self.add(A_eq_two_tex)
-
-        A_eq_three_tex=MathTex(r"A=\pi R^{2}")
-        A_eq_three_tex.move_to(A_eq_two_tex)
-        self.play(
-            ReplacementTransform(A_eq_two_tex,A_eq_three_tex)
-        )
-        self.wait(5)
 
         x_squared_line_label=MathTex(r"y", r"=", r"x", r"^2", color=YELLOW)
-        x_squared_line_label.move_to(line_label.get_center())
-        self.play(
-            FadeOut(line_label[3]),
-            run_time=0.01
-        )
-        self.wait(0.5)
-        self.play(
-            line_label[2].animate.move_to(x_squared_line_label[3].get_center()).scale(0.8),
-            Transform(line_label[0], x_squared_line_label[0]),
-            Transform(line_label[1], x_squared_line_label[1]),
-            Transform(line_label[4], x_squared_line_label[2]),
-        )
-        self.remove(line_label)
+        x_squared_line_label.shift(LEFT)
+        
         self.add(x_squared_line_label)
 
-        self.wait(1.5)
         line_x_two = axes.plot(
             lambda x: x**2, 
             x_range=[0, 9], 
             color=YELLOW
         )
         A_eq_x_two_start=MathTex(r"A")
-        A_eq_x_two_start.move_to(A_eq_three_tex)
+        A_eq_x_two_start.shift(RIGHT*2.5)
 
         new_thickness = 0.2
 
-        animations = []
+        
         for i in range(num_rings):
             x_val = (i + 0.5) * new_thickness
             new_math_height = x_val**2
@@ -169,25 +97,38 @@ class demo(Scene):
             target_top_point = axes.c2p(x_val, new_math_height)
             target_bottom_point = axes.c2p(x_val, 0)
             target_physical_height = target_top_point[1] - target_bottom_point[1]
-            animations.append(
-                bars[i].animate.stretch_to_fit_height(
-                    target_physical_height, 
-                    about_edge=DOWN
-                ).stretch_to_fit_width(
-                    new_thickness, 
-                    about_edge=LEFT # Keeps bars aligned to their left neighbors
-                ).move_to(target_bottom_point, aligned_edge=DOWN)
-            )
+            
+            bars[i].stretch_to_fit_height(
+                target_physical_height, 
+                about_edge=DOWN
+            ).stretch_to_fit_width(
+                new_thickness, 
+                about_edge=LEFT # Keeps bars aligned to their left neighbors
+            ).move_to(target_bottom_point, aligned_edge=DOWN)
+            
+        self.add(A_eq_x_two_start)
+        self.add(bars)
+        self.add(line_x_two)
+        
         self.play(
-            LaggedStart(*animations, lag_ratio=0.05),
-            ReplacementTransform(line,line_x_two),
-            ReplacementTransform(A_eq_three_tex,A_eq_x_two_start),
-            FadeOut(circle),
-            Unwrite(label),
-            Unwrite(c_label),
-            Unwrite(r_label),
-            run_time=2
+            bars[-2].animate.set_stroke(color=RED, width=4),
+            run_time=1
         )
+        self.wait(4)
+
+        dA_eq_tex=MathTex(r"dA=x^2 \cdot dx")
+        dA_eq_tex.shift(RIGHT*3.6+UP)
+        self.play(
+            Write(dA_eq_tex)
+        )
+        self.wait(4)
+        dA_eq_two_tex=MathTex(r"\frac{dA}{dx}=x^2")
+        dA_eq_two_tex.shift(RIGHT*3.3+UP)
+
+        self.play(
+            ReplacementTransform(dA_eq_tex,dA_eq_two_tex)
+        )
+
         self.wait(1)
         
 
