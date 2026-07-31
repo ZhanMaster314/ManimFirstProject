1d10df1774706f33b4ebde82ecf061374cdb421d
ZhanMaster314 <thesecondcoming82@gmail.com>
2026-07-31 15:01:12 +0500

    Piston & Partition

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from manim import *
+from manim import * 
 import copy
 import math
 import numpy as np
@@ -15,121 +15,164 @@ DEFAULT_MOBJECT_TO_MOBJECT_BUFF=0.25
 
 class demo(Scene):
     def construct(self):
-        radius=1.5
-        num_rings = 15
-    
+        left_origin = LEFT * 3.5
         
-        #BARS INTRO
-        thickness = radius / num_rings
-        bars = VGroup()
-        for i in range(num_rings):
-            # Circumference = 2 * PI * radius
-            # We use the average radius of the ring for accuracy
-            avg_radius = (i * thickness + (i + 1) * thickness) / 2
-            length = 2 * PI * avg_radius
-            
-            bar = Rectangle(
-                width=thickness, 
-                height=length, 
-                fill_color=BLUE, 
-                fill_opacity=0.5,
-                stroke_width=0
-                )
-            bars.add(bar)
+        # Cylinder outer walls (closed left end, open right end)
+        cyl_left_walls = VGroup(
+            Line(left_origin + LEFT * 2 + UP * 1.2, left_origin + RIGHT * 1.8 + UP * 1.2),
+            Line(left_origin + LEFT * 2 + DOWN * 1.2, left_origin + RIGHT * 1.8 + DOWN * 1.2),
+            Line(left_origin + LEFT * 2 + UP * 1.2, left_origin + LEFT * 2 + DOWN * 1.2),
+        ).set_stroke(BLUE_C, width=4)
+
+        # Thick Piston Head (solid block)
+        piston_head = Rectangle(
+            height=2.32, 
+            width=0.8, 
+            fill_opacity=0.85, 
+            fill_color=GRAY_B, 
+            stroke_color=WHITE,
+            stroke_width=2
+        ).move_to(left_origin + LEFT * 0.4)
+
+        # Piston Shaft/Rod
+        piston_rod = Rectangle(
+            height=0.35, 
+            width=2.2, 
+            fill_opacity=0.9, 
+            fill_color=LIGHT_GRAY, 
+            stroke_color=WHITE,
+            stroke_width=2
+        ).next_to(piston_head, RIGHT, buff=0)
+
+        left_system = VGroup(cyl_left_walls, piston_head, piston_rod)
         
+        right_origin = RIGHT * 3.5
+
+        # Enclosed Cylinder walls (closed both ends)
+        cyl_right_walls = VGroup(
+            Line(right_origin + LEFT * 2 + UP * 1.2, right_origin + RIGHT * 2 + UP * 1.2),
+            Line(right_origin + LEFT * 2 + DOWN * 1.2, right_origin + RIGHT * 2 + DOWN * 1.2),
+            Line(right_origin + LEFT * 2 + UP * 1.2, right_origin + LEFT * 2 + DOWN * 1.2),
+            Line(right_origin + RIGHT * 2 + UP * 1.2, right_origin + RIGHT * 2 + DOWN * 1.2),
+        ).set_stroke(BLUE_C, width=4)
+
+        # Thin Disc Partition inside
+        partition = Rectangle(
+            height=2.32, 
+            width=0.08, 
+            fill_opacity=1.0, 
+            fill_color=RED_D, 
+            stroke_color=RED_A,
+            stroke_width=1.5
+        ).move_to(right_origin)
+
+        right_system = VGroup(cyl_right_walls, partition)
         
-        
-        axes = Axes(
-            x_range=[-1, 9, 1], # Range from -1 to 12
-            y_range=[-1, 9, 1], # Range from -1 to 10
-            x_length=8,         # Total width in units
-            y_length=8,          # Total height in units
-            axis_config={
-                "color": GREY,
-                "stroke_width": 2,
-                "include_numbers": False, # Keep it clean
-                "include_tip": True,
-            },
-            tips=True,
-        )
-        axes.move_to(RIGHT * 3 + UP * 2)
-
-        x_label = axes.get_x_axis_label("x")
-        y_label = axes.get_y_axis_label("y")
-
-        self.add(axes)
-        self.add(x_label)
-        self.add(y_label)
-
-        
-        origin_coord = axes.c2p(0, 0) 
-        
-        y_shrinking_group = VGroup(bars, axes.get_y_axis())
-        y_scale_factor = 1 / (2 )
-        y_shrinking_group.scale(
-                [1, y_scale_factor, 1], 
-                about_point=origin_coord
-            )
-
-        
-
-        x_squared_line_label=MathTex(r"y", r"=", r"x", r"^2", color=YELLOW)
-        x_squared_line_label.shift(LEFT)
-        
-        self.add(x_squared_line_label)
-
-        line_x_two = axes.plot(
-            lambda x: x**2, 
-            x_range=[0, 9], 
-            color=YELLOW
-        )
-        A_eq_x_two_start=MathTex(r"A")
-        A_eq_x_two_start.shift(RIGHT*2.5)
-
-        new_thickness = 0.2
-
-        
-        for i in range(num_rings):
-            x_val = (i + 0.5) * new_thickness
-            new_math_height = x_val**2
-
-
-            target_top_point = axes.c2p(x_val, new_math_height)
-            target_bottom_point = axes.c2p(x_val, 0)
-            target_physical_height = target_top_point[1] - target_bottom_point[1]
-            
-            bars[i].stretch_to_fit_height(
-                target_physical_height, 
-                about_edge=DOWN
-            ).stretch_to_fit_width(
-                new_thickness, 
-                about_edge=LEFT # Keeps bars aligned to their left neighbors
-            ).move_to(target_bottom_point, aligned_edge=DOWN)
-            
-        self.add(A_eq_x_two_start)
-        self.add(bars)
-        self.add(line_x_two)
-        
-        self.play(
-            bars[-2].animate.set_stroke(color=RED, width=4),
-            run_time=1
-        )
-        self.wait(4)
-
-        dA_eq_tex=MathTex(r"dA=x^2 \cdot dx")
-        dA_eq_tex.shift(RIGHT*3.6+UP)
-        self.play(
-            Write(dA_eq_tex)
-        )
-        self.wait(4)
-        dA_eq_two_tex=MathTex(r"\frac{dA}{dx}=x^2")
-        dA_eq_two_tex.shift(RIGHT*3.3+UP)
-
-        self.play(
-            ReplacementTransform(dA_eq_tex,dA_eq_two_tex)
-        )
-
         self.wait(1)
+        self.play(
+            Create(left_system),
+            Create(right_system),
+            run_time=2
+            )
+        self.wait(3)
+        self.play(
+            FadeOut(left_system),
+            FadeOut(right_system),
+            run_time=2
+            )
+        
+        def create_cog(radius=0.25, teeth=6, color=LIGHT_GRAY):
+            circle = Circle(radius=radius, color=color, fill_opacity=0.8, fill_color=LIGHT_GRAY)
+            hole = Circle(radius=radius * 0.4, color=WHITE, fill_opacity=1.0, fill_color=BLACK)
+            t_list = []
+            for i in range(teeth):
+                angle = i * (2 * PI / teeth)
+                tooth = Square(side_length=radius * 0.35, color=color, fill_opacity=0.8, fill_color=DARK_GRAY)
+                tooth.move_to([
+                    (radius + 0.05) * np.cos(angle),
+                    (radius + 0.05) * np.sin(angle),
+                    0
+                ])
+                tooth.rotate(angle)
+                t_list.append(tooth)
+            return VGroup(*t_list, circle, hole)
+
+        cog1 = create_cog(0.22, 6, GRAY_A)
+        cog2 = create_cog(0.15, 6, GRAY_A).move_to(cog1.get_center() + RIGHT * 0.3 + UP * 0.2)
+        cogs_icon = VGroup(cog1, cog2).scale(0.8)
+
+        # 2. Water Pool Icon (Hydrostatics)
+        pool_base = Ellipse(width=0.6, height=0.25, color=BLUE_D, fill_opacity=0.8)
+
+        # Constructing a drop using a Dot and a Triangle
+        drop_base = Dot(radius=0.08, color=BLUE_B)
+        drop_tip = (
+            Triangle(color=BLUE_B, fill_opacity=1.0, stroke_width=0)
+        .scale(0.09)
+        .move_to(drop_base.get_center() + UP * 0.07)
+        )
+        drop = VGroup(drop_base, drop_tip)
+        drop.move_to(pool_base.get_center() + UP * 0.2)
+
+        water_icon = VGroup(pool_base, drop)
+        # 3. Molecule Icon (Molecular Physics)
+        atom_center = Dot(radius=0.08, color=RED_C)
+        atom1 = Dot(radius=0.05, color=WHITE).move_to(UP * 0.2 + LEFT * 0.18)
+        atom2 = Dot(radius=0.05, color=WHITE).move_to(DOWN * 0.2 + RIGHT * 0.15)
+        atom3 = Dot(radius=0.05, color=WHITE).move_to(UP * 0.05 + RIGHT * 0.22)
+        
+        bonds = VGroup(
+            Line(atom_center.get_center(), atom1.get_center(), stroke_width=2, color=GRAY),
+            Line(atom_center.get_center(), atom2.get_center(), stroke_width=2, color=GRAY),
+            Line(atom_center.get_center(), atom3.get_center(), stroke_width=2, color=GRAY),
+        )
+        molecule_icon = VGroup(bonds, atom_center, atom1, atom2, atom3)
+
+        # 4. Thermometer Icon (Thermodynamics)
+        bulb = Circle(radius=0.1, color=WHITE, fill_opacity=1.0, fill_color=RED_E)
+        stem = Rectangle(height=0.35, width=0.1, color=WHITE, fill_opacity=1.0, fill_color=WHITE)
+        stem.next_to(bulb, UP, buff=-0.04)
+        mercury = Rectangle(height=0.22, width=0.04, color=RED_E, fill_opacity=1.0, stroke_width=0)
+        mercury.move_to(stem.get_bottom() + UP * 0.11)
+        thermometer_icon = VGroup(bulb, stem, mercury)
+        
+        data = [
+            ("Mechanics", cogs_icon),
+            ("Hydrostatics", water_icon),
+            ("Molecular Physics", molecule_icon),
+            ("Thermodynamics", thermometer_icon),
+        ]
+
+        list_group = VGroup()
+
+        for text_str, icon in data:
+            label = Text(text_str, font_size=32, color=WHITE)
+            # Align icon to the left of the text
+            icon.next_to(label, LEFT, buff=0.4)
+            item = VGroup(icon, label)
+            list_group.add(item)
+
+        # Arrange list items vertically with left alignment
+        list_group.arrange(DOWN, aligned_edge=LEFT, buff=0.6)
+        list_group.center()
+
+        for item in list_group:
+            icon, label = item[0], item[1]
+            self.play(
+                FadeIn(icon, shift=RIGHT * 0.2),
+                Write(label),
+                run_time=0.9
+            )
+        self.wait(1.4)
+        for item in list_group:
+            icon, label = item[0], item[1]
+            self.play(
+                FadeOut(icon, shift=RIGHT * 0.2),
+                Unwrite(label),
+                run_time=0.2
+            )
+        self.wait(0.2)
+
         
 
 
