2727a1a72fb0f408cc2befcaab0b4fd5e7b48c81
ZhanMaster314 <thesecondcoming82@gmail.com>
2026-08-31 10:02:48 +0500

    Polyhedra 1 done

--- main.py
+++ main.py
@@ -15,274 +15,353 @@ DEFAULT_MOBJECT_TO_MOBJECT_BUFF=0.25
 
 class demo(Scene):
     def construct(self):
-         # 2. Water Pool Icon (Hydrostatics)
-        pool_base = Ellipse(width=0.6, height=0.25, color=BLUE_D, fill_opacity=0.8)
+        self.wait(5)
+        eueler_photo=ImageMobject("assets/euler.png")
+        coshi_photo=ImageMobject("assets/Cauchy-Portrait.jpg")
+        alexandrov_photo=ImageMobject("assets/Aleksandrov.jpg")
 
-        # Constructing a drop using a Dot and a Triangle
-        drop_base = Dot(radius=0.08, color=BLUE_B)
-        drop_tip = (
-            Triangle(color=BLUE_B, fill_opacity=1.0, stroke_width=0)
-        .scale(0.09)
-        .move_to(drop_base.get_center() + UP * 0.07)
-        )
-        drop = VGroup(drop_base, drop_tip)
-        drop.move_to(pool_base.get_center() + UP * 0.2)
+        for img in [eueler_photo,coshi_photo,alexandrov_photo]:
+            img.height=2.5
+        
+        img_row=Group(eueler_photo,coshi_photo,alexandrov_photo)
+        img_row.arrange(RIGHT, buff=0.8)
+        img_row.center()
 
-        water_icon = VGroup(pool_base, drop)
-
-        hydrostatics_label = Text("Hydrostatics", font_size=32, color=WHITE)
-        water_icon.next_to(hydrostatics_label, LEFT, buff=0.4)
-        self.wait(0.4)
-        self.play(
-            Write(hydrostatics_label),
-            FadeIn(water_icon),
-            run_time=1
+        frame1=SurroundingRectangle(
+            eueler_photo,
+            color=BLUE_C,
+            buff=0.05,
+            corner_radius=0.1,
+            stroke_width=3
         )
-        self.wait(1)
-        self.play(
-            Unwrite(hydrostatics_label),
-            FadeOut(water_icon),
-            run_time=1
+        frame2=SurroundingRectangle(
+            coshi_photo,
+            color=BLUE_C,
+            buff=0.05,
+            corner_radius=0.1,
+            stroke_width=3
         )
-        problem_1_label = Text("Problem 1", font_size=32, color=WHITE)
-        problem_1_label.to_corner(UL)
+        frame3=SurroundingRectangle(
+            alexandrov_photo,
+            color=BLUE_C,
+            buff=0.05,
+            corner_radius=0.1,
+            stroke_width=3
+        )
+        frames=VGroup(frame1,frame2,frame3)
         self.play(
-            Write(problem_1_label),
+            FadeIn(eueler_photo),
+            FadeIn(coshi_photo),
+            FadeIn(alexandrov_photo),
+            Create(frames),
+            run_time=2.8
+        )
+        self.wait(2.8)
+        self.wait(4.3)
+        self.play(
+            FadeOut(frames),
+            FadeOut(img_row),
             run_time=2
         )
-        self.wait(2.2)
-
-        #Draw water pool
-        bottom_of_the_pool=Line(LEFT * 2 + DOWN * 1.2,  RIGHT * 2 + DOWN * 1.2)
-        water_pool_walls = VGroup(
-            Line(LEFT * 2 + UP * 1.2,  LEFT * 2 + DOWN * 1.2),
-            bottom_of_the_pool,
-            Line( RIGHT * 2 + UP * 1.2,  RIGHT * 2 + DOWN * 1.2),
-        ).set_stroke(WHITE, width=4)
-        self.play(
-            Create(water_pool_walls),
-            run_time=2.2
+        minkowski_photo=ImageMobject("assets/Minkowski.jpg")
+        minkowski_photo.height=3
+        frame4=SurroundingRectangle(
+            minkowski_photo,
+            color=BLUE_C,
+            buff=0.05,
+            corner_radius=0.1,
+            stroke_width=3
         )
-        #highlight bottom
+        label_1897=MathTex("1897",font_size=50).next_to(minkowski_photo, DOWN,buff=0.2)
         self.play(
-            bottom_of_the_pool.animate.set_color(YELLOW),
-            run_time=0.7
+            Write(label_1897),
+            run_time=2
         )
-        area_s_label=MathTex(r"S=15m^{2}", font_size=32, color=WHITE).next_to(bottom_of_the_pool,DOWN,0.5)
-
+        self.wait(2)
         self.play(
-            Write(area_s_label),
-            run_time=3
-        )
-        self.play(
-            bottom_of_the_pool.animate.set_color(WHITE),
+            FadeIn(minkowski_photo),
             run_time=1
         )
-        self.wait(1.3)
-        #Draw water
-        left_water_rect=Rectangle(
-            height=1.2, 
-            width=2, 
-            fill_opacity=1.0,
-            fill_color=BLUE_C,
-            stroke_width=0
-        ).shift(LEFT+DOWN*0.6)
-        right_water_rect=Rectangle(
-            height=1.2, 
-            width=2, 
-            fill_opacity=1.0,
-            fill_color=BLUE_C,
-            stroke_width=0
-        ).shift(RIGHT+DOWN*0.6)
+        self.wait(2.4)
         self.play(
-            Create(left_water_rect),
-            Create(right_water_rect),
-            run_time=1.8
-        )
-        #write h label
-        h_label=Text("h=1m", font_size=32, color=WHITE).next_to(right_water_rect,RIGHT,2)
-        self.play(
-            Write(h_label),
+            Unwrite(label_1897),
+            FadeOut(minkowski_photo),
             run_time=1
         )
-        self.wait(2.3)
-        #Draw partition
-        partition = Rectangle(
-            height=2.32, 
-            width=0.08, 
-            fill_opacity=1.0, 
-            fill_color=RED_D, 
-            stroke_color=RED_A,
-            stroke_width=1.5
-        )
-        partition.set_z_index(1)
+        title1_label=Text("Convex polyhedra and their hedgehogs",font_size=50)
         self.play(
-           Create(partition) ,
-           run_time=3
+            Write(title1_label),
+            run_time=2.9
         )
-        self.wait(2.7)
-        #A ? label
-        A_question_label=Text("A=?", font_size=32, color=WHITE).next_to(left_water_rect,LEFT,2)
+        self.wait(4.9)
         self.play(
-            Write(A_question_label),
+            FadeOut(title1_label),
             run_time=1
         )
-        self.wait(2.3)
-        #Move partition
-        left_water_rect.save_state()
-        right_water_rect.save_state()
 
-        final_left_water_rect=Rectangle(
-            height=2.4, 
-            width=1, 
-            fill_opacity=1.0,
-            fill_color=BLUE_C,
-        ).shift(LEFT*1.5)
-        final_right_water_rect=Rectangle(
-            height=0.8, 
-            width=3, 
-            fill_opacity=1.0,
-            fill_color=BLUE_C,
-        ).shift(RIGHT*0.5+DOWN*0.8)
+class Polyhedron3D(ThreeDScene):
+    def construct(self):
+        self.set_camera_orientation(phi=75 *DEGREES,theta=-45*DEGREES)
+
+        polyhedron=Icosahedron(edge_length=2.0)
+        polyhedron.set_fill(BLUE_D,opacity=0.8)
+        polyhedron.set_stroke(WHITE,width=1.5)
+
         self.play(
-                partition.animate.shift(LEFT),
-                Transform(left_water_rect,final_left_water_rect),
-                Transform(right_water_rect,final_right_water_rect),
-                run_time=3
+            Create(polyhedron),
+            run_time=1.2
+        )
+        #HighLight polyhedrons body/border
+        self.play(
+            polyhedron.faces.animate.set_fill(YELLOW_D,opacity=0.8),
+            run_time=1
             )
-        self.wait(2.2)
-        #Write 1:3
-        one_to_three_label=Text("1 : 3", font_size=32, color=WHITE).next_to(area_s_label,DOWN,1)
+        self.wait(0.3)
+
         self.play(
-            Write(one_to_three_label),
+            polyhedron.faces.animate.set_fill(BLUE_D,opacity=0.8),
+            run_time=1
+            )
+        self.play(
+            polyhedron.graph.animate.set_stroke(YELLOW_D,width=1.5),
+            run_time=1
+            )
+        self.wait(0.6)
+        self.play(
+            FadeOut(polyhedron),
+            run_time=1
+        )
+        self.wait(1.8)
+        #Drawing planes
+        tetrahedron=Tetrahedron(edge_length=3.0)
+        tetrahedron.set_fill(BLUE_D,opacity=0.8)
+
+        vertices_dict=tetrahedron.vertex_coords
+        vertices=list(vertices_dict)
+
+        face_indices=[
+            [0,1,2],
+            [0,1,3],
+            [0,2,3],
+            [1,2,3]
+        ]
+        planes=VGroup()
+        colors=[RED_C,GREEN_C,YELLOW_C,PURPLE_C]
+
+        for idx, face in enumerate(face_indices):
+            p1,p2,p3=vertices[face[0]],vertices[face[1]],vertices[face[2]]
+            center=(p1+p2+p3)/3
+
+            plane=Polygon(
+                center+1.8*(p1-center),
+                center+1.8*(p2-center),
+                center+1.8*(p3-center),
+                fill_color=colors[idx],
+                fill_opacity=0.35,
+                stroke_color=colors[idx],
+                stroke_width=2
+            )
+            planes.add(plane)
+        
+        for plane in planes:
+            self.play(
+                Create(plane),
+                run_time=1.1
+            )
+        #Camera move 1
+        self.move_camera(phi=80 * DEGREES, theta=140 * DEGREES, run_time=7.5)
+        #Marking H+i
+        H_plus_i_label=MathTex(r"H_i^+",font_size=54,color=YELLOW)
+        H_plus_i_label.to_corner(DR, buff=0.6)
+
+        self.add_fixed_in_frame_mobjects(H_plus_i_label)
+        self.play(
+            FadeIn(tetrahedron),
+            Write(H_plus_i_label),
+            run_time=1
+        )
+        self.wait(1.2)
+        #Marking H-i
+        H_minus_i_label=MathTex(r"H_i^-",font_size=54,color=YELLOW)
+        H_minus_i_label.to_corner(DL, buff=0.6)
+
+        self.add_fixed_in_frame_mobjects(H_minus_i_label)
+        self.play(
+            FadeOut(tetrahedron),
+            Write(H_minus_i_label),
             run_time=1
         )
         self.wait(3)
         self.play(
-            Unwrite(one_to_three_label),
-            run_time=1
-        )
-        #Move partition back
-        self.play(
-                partition.animate.shift(RIGHT),
-                Restore(left_water_rect),
-                Restore(right_water_rect),
-                run_time=3
-            )
-        self.wait(4.2)
-        #write E beg
-        E_beg_label=MathTex(r"E_{\text{beg}}", font_size=48).shift(DOWN*3+LEFT*3)
-        E_fin_label=MathTex(r"E_{\text{fin}}", font_size=48).shift(DOWN*3+RIGHT)
-        self.play(
-            Write(E_beg_label),
-            run_time=1
-        )
-        self.wait(2.3)
-        self.play(
-            Write(E_fin_label),
-            run_time=1
-        )
-        self.wait(7.8)
-        #write =mgh/2
-        E_beg_value_label=MathTex(r"= \frac{mgh}{2}", font_size=48)
-        E_beg_value_label.next_to(E_beg_label, RIGHT, buff=0.15)
-        self.play(
-            Write(E_beg_value_label),
-            run_time=3
-        )
-        self.wait(4.2)
-        #write energy final 
-        E_fin_value_label=MathTex(r"= \frac{m}{2} g \frac{h_1}{2} + \frac{m}{2} g \frac{h_2}{2}", font_size=48)
-        E_fin_value_label.next_to(E_fin_label, RIGHT, buff=0.15)
-        self.play(
-            Write(E_fin_value_label),
-            run_time=4
-        )
-        self.wait(12.8)
-        #Write m label
-        m_label=MathTex(r"m = \rho S h", font_size=48)
-        m_label.next_to(h_label,UP,buff=0.8)
-        self.play(
-            Write(m_label),
-            run_time=3
-        )
-        self.wait(5.6)
-        #Move partition
-        self.play(
-                partition.animate.shift(LEFT),
-                Transform(left_water_rect,final_left_water_rect),
-                Transform(right_water_rect,final_right_water_rect),
-                run_time=1
-            )
-        #Write h1 h2
-        h1_label=MathTex(r"h_1", font_size=48)
-        h1_label.next_to(final_left_water_rect, UP, buff=0.2)
-
-        h2_label=MathTex(r"h_2", font_size=48)
-        h2_label.next_to(final_right_water_rect, UP, buff=0.2)
-        self.play(
-            Write(h1_label),
-            Write(h2_label),
-            run_time=1
-        )
-        self.wait(9.7)
-        #text fade out
-        self.play(
-            Unwrite(area_s_label),
-            Unwrite(h_label),
-            Unwrite(A_question_label),
-            Unwrite(E_beg_label),
-            Unwrite(E_beg_value_label),
-            Unwrite(E_fin_label),
-            Unwrite(E_fin_value_label),
-            run_time=1
-        )
-        #V=const
-        V_const_label=MathTex(r"V = const", font_size=48).shift(DOWN*3+LEFT*4)
-        self.play(
-            Write(V_const_label),
-            run_time=1
-        )
-        self.wait(2.3)
-        hS_equation=MathTex(r"\frac{hS}{2} =\frac{h_1 S}{4} =\frac{h_2 3S}{4}", font_size=48)
-        hS_equation.next_to(V_const_label,RIGHT, buff=1)
-        self.play(
-            Write(hS_equation),
-            run_time=3
-        )
-        self.wait(14)
-        self.play(
-            Unwrite(hS_equation),
-            Unwrite(V_const_label),
+            FadeIn(tetrahedron),
             run_time=1.5
         )
-        #A=E_fin-E_beg
-        A_label=MathTex(r"A=", font_size=48).shift(DOWN*3+LEFT*1)
-        A_value_1_label=MathTex(r"E_{\text{fin}}-E_{\text{beg}}", font_size=48).next_to(A_label,RIGHT,buff=0.2)
-        A_value_2_label=MathTex(r"\frac{\rho g S h^{2}}{6}", font_size=48).next_to(A_label,RIGHT,buff=0.2)
-        A_value_3_label=MathTex(r"25 kJ", font_size=48).next_to(A_label,RIGHT,buff=0.2)
-
-
         self.play(
-            Write(A_label),
-            Write(A_value_1_label),
-            run_time=3
+            FadeOut(tetrahedron),
+            Unwrite(H_plus_i_label),
+            Unwrite(H_minus_i_label),
+            run_time=1.5
         )
-        self.wait(3.3)
-        self.play(
-            ReplacementTransform(A_value_1_label,A_value_2_label),
-            run_time=3
+        self.wait(0.6)
+        #intersection h+i 
+        intersection_H_plus_i_label=MathTex(r"\cap H_i^+",font_size=54,color=YELLOW)
+        intersection_H_plus_i_label.to_corner(DR, buff=0.6)
 
-        )
-        self.wait(3.2)
+        self.add_fixed_in_frame_mobjects(intersection_H_plus_i_label)
         self.play(
-            ReplacementTransform(A_value_2_label,A_value_3_label),
+            Write(intersection_H_plus_i_label),
             run_time=1
+        )
+        self.wait(8.5)
+        #Draw sphere around tetrahedron
+        centroid=np.mean(vertices,axis=0)
+        radius=3.0 *np.sqrt(3/8)
 
+        sphere=Sphere(center=centroid,radius=radius)
+        sphere.set_fill(YELLOW, opacity=0.25)
+        
+        self.play(
+            Create(sphere),
+            run_time=1
+        )
+        self.wait(1.6)
+        self.play(
+            FadeOut(sphere),
+            FadeOut(planes),
+            run_time=1
+        )
+        #Draw multifaceted angle
+        for plane in planes[:3]:
+            self.play(Create(plane),run_time=0.666)
+        #Camera move 2
+        self.move_camera(phi=75 * DEGREES, theta=-45 * DEGREES, run_time=5.3)
+
+        self.play(Create(planes[3]),run_time=1)
+
+        self.wait(3.5)
+        self.play(
+            FadeIn(tetrahedron),
+            run_time=1
+        )
+        self.wait(2.6)
+        self.play(
+            FadeOut(tetrahedron),
+            FadeOut(planes),
+            run_time=1
         )
         self.wait(3)
+        #Supporting plane intro
+        horizontal_plane=NumberPlane(
+            x_range=[-4,4,1],
+            y_range=[-4,4,1],
+            background_line_style={
+                "stroke_color":TEAL,
+                "stroke_width":1,
+                "stroke_opacity":0.4
+            }
+        )
+        plane_fill=Square(side_length=8,fill_color=TEAL_E,fill_opacity=0.2,stroke_width=0)
+        supporting_plane_group=VGroup(plane_fill,horizontal_plane)
 
+        self.play(
+            Create(supporting_plane_group),
+            run_time=1
+        )
+        self.wait(3.9)
+        #create pentahedron (Square pyramid)
+        height=2.5
+        base_size=2.0
 
+        apex=np.array([0,0,0])
+        v1=np.array([-base_size/2,-base_size/2,height])
+        v2=np.array([base_size/2,-base_size/2,height])
+        v3=np.array([base_size/2,base_size/2,height])
+        v4=np.array([-base_size/2,base_size/2,height])
+
+        vertex_coords=[apex,v1,v2,v3,v4]
+
+        faces_list=[
+            [0,1,2],
+            [0,2,3],
+            [0,3,4],
+            [0,4,1],
+            [1,2,3,4]
+        ]
+
+        pentahedron=Polyhedron(
+            vertex_coords,
+            faces_list,
+            faces_config={"fill_color":GOLD_E,"fill_opacity":0.7,"stroke_color":WHITE,"stroke_width":1.5},
+            graph_config={"vertex_config":{"radius":0.05,"color":RED}}
+        )
+        self.play(
+            FadeIn(pentahedron,shift=DOWN*0.5),
+            run_time=2
+        )
+        self.wait(13.5)
+        #High light the point
+        touching_point=Dot3D(point=apex,radius=0.08,color=RED)
+        self.play(
+            Create(touching_point),
+            run_time=1
+        )
+        self.wait(3)
+        #plane which contains full segment 
+        edge_vec=v1-apex
+        side_vec=np.array([1,-1,0])
+
+        plane_containing_edge=Polygon(
+            apex-0.5*edge_vec+3*side_vec,
+            apex-0.5*edge_vec-3*side_vec,
+            v1+0.5*edge_vec-3*side_vec,
+            v1+0.5*edge_vec+3*side_vec,
+            fill_color=TEAL_E,
+            fill_opacity=0.3,
+            stroke_color=TEAL,
+            stroke_width=1.5
+        )
+        contact_edge=Line3D(start=apex,
+                            end=v1,
+                            color=RED,
+                            thickness=0.03
+                            )
+        self.play(
+            Transform(horizontal_plane,plane_containing_edge),
+            FadeIn(contact_edge),
+            run_time=3
+        )
+        self.wait(1.1)
+        #plane which contains full face
+        face_center = (apex + v1 + v2) / 3
+
+        p_apex = face_center + 2.2 * (apex - face_center)
+        p_v1   = face_center + 2.2 * (v1 - face_center)
+        p_v2   = face_center + 2.2 * (v2 - face_center)
+
+        plane_face = Polygon(
+            p_apex,
+            p_v1,
+            p_v2,
+            fill_color=TEAL_E,
+            fill_opacity=0.35,
+            stroke_color=TEAL,
+            stroke_width=1.5
+        )
+        contact_face = Polygon(
+            apex, v1, v2,
+            fill_color=RED,
+            fill_opacity=0.5,
+            stroke_color=RED_A,
+            stroke_width=2
+        )
+        self.play(
+            FadeOut(contact_edge),
+            Transform(plane_containing_edge,plane_face),
+            FadeIn(contact_face),
+            run_time=3
+        )
+        self.wait(3)
+        
 
 
 
